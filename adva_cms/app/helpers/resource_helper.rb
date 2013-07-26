5f430c0235d4cf67a4353caeac4e5c19ec226d30
Micah Geisel <micah@botandrose.com>
2013-07-26 18:00:07 -0500

    publicize ResourceHelper private methods.

--- adva_cms/app/helpers/resource_helper.rb
+++ adva_cms/app/helpers/resource_helper.rb
@@ -53,77 +53,75 @@ module ResourceHelper
     actions.map { |action| resource_link(action, *args) }.join("\n")
   end
 
-  protected
+  def normalize_resource_type(action, type, resource)
+    type ||= resource.is_a?(Symbol) ? resource : resource.class.name
+    type = 'section' if type.to_s.classify.constantize < Section
+    type = type.to_s.tableize.gsub("/","_") if action == :index
+    type = type.to_s.split("::").first == "Adva" ? type.to_s.underscore.gsub("/","_") : type.to_s.demodulize.underscore
+    type
+  end
 
-    def normalize_resource_type(action, type, resource)
-      type ||= resource.is_a?(Symbol) ? resource : resource.class.name
-      type = 'section' if type.to_s.classify.constantize < Section
-      type = type.to_s.tableize.gsub("/","_") if action == :index
-      type = type.to_s.split("::").first == "Adva" ? type.to_s.underscore.gsub("/","_") : type.to_s.demodulize.underscore
-      type
+  def resource_url_namespace(options)
+    options.key?(:namespace) ? options.delete(:namespace) : current_controller_namespace
+  end
+
+  def current_controller_namespace
+    path = respond_to?(:controller_path) ? controller_path : controller.controller_path
+    namespace = path.split('/')[0..-2].join('_')
+    namespace.present? ? namespace : nil
+  end
+
+  def resource_link_id(action, type, resource)
+    id = [action, type]
+    id << resource.id if resource.is_a?(ActiveRecord::Base) && !action.in?(:index, :new)
+    id.join('_')
+  end
+
+  def resource_owners(resource)
+    return [] if resource.nil? || resource.is_a?(Symbol)
+    return resource.owners << resource if resource.respond_to?(:owners)
+
+    owners = []
+    if resource.respond_to?(:section)
+      owners << resource.section.site << resource.section
+    elsif resource.respond_to?(:site)
+      owners << resource.site
+    elsif resource.respond_to?(:owner)
+      owners << resource.owner
     end
 
-    def resource_url_namespace(options)
-      options.key?(:namespace) ? options.delete(:namespace) : current_controller_namespace
-    end
+    owners << resource
+  end
 
-    def current_controller_namespace
-      path = respond_to?(:controller_path) ? controller_path : controller.controller_path
-      namespace = path.split('/')[0..-2].join('_')
-      namespace.present? ? namespace : nil
-    end
+  def resource_url_method(namespace, action, type, options)
+    method = [namespace, type].compact
 
-    def resource_link_id(action, type, resource)
-      id = [action, type]
-      id << resource.id if resource.is_a?(ActiveRecord::Base) && !action.in?(:index, :new)
-      id.join('_')
-    end
+    method << (options.delete(:only_path) ? 'path' : 'url')
+    method.unshift(action) if [:new, :edit].include?(action.to_sym)
 
-    def resource_owners(resource)
-      return [] if resource.nil? || resource.is_a?(Symbol)
-      return resource.owners << resource if resource.respond_to?(:owners)
+    method.compact.join('_').gsub('/', '_')
+  end
 
-      owners = []
-      if resource.respond_to?(:section)
-        owners << resource.section.site << resource.section
-      elsif resource.respond_to?(:site)
-        owners << resource.site
-      elsif resource.respond_to?(:owner)
-        owners << resource.owner
-      end
+  def normalize_resource_link_options(options, action, type, resource)
+    options[:class] ||= "#{action} #{type}"
+    options[:id] ||= resource_link_id(action, type, resource)
+    options[:title] ||= t(:"adva.titles.#{action}")
+    options.reverse_merge!(resource_delete_options(type, options)) if action == :delete
+    options
+  end
 
-      owners << resource
-    end
+  def normalize_resource_link_text(text, action, type)
+    type = type.to_s.gsub('/', '_').pluralize
+    text ||= t(:"adva.#{type}.links.#{action}", :default => :"adva.resources.links.#{action}")
+    text = t(text) if text.is_a?(Symbol)
+    text
+  end
 
-    def resource_url_method(namespace, action, type, options)
-      method = [namespace, type].compact
-
-      method << (options.delete(:only_path) ? 'path' : 'url')
-      method.unshift(action) if [:new, :edit].include?(action.to_sym)
-
-      method.compact.join('_').gsub('/', '_')
-    end
-
-    def normalize_resource_link_options(options, action, type, resource)
-      options[:class] ||= "#{action} #{type}"
-      options[:id] ||= resource_link_id(action, type, resource)
-      options[:title] ||= t(:"adva.titles.#{action}")
-      options.reverse_merge!(resource_delete_options(type, options)) if action == :delete
-      options
-    end
-
-    def normalize_resource_link_text(text, action, type)
-      type = type.to_s.gsub('/', '_').pluralize
-      text ||= t(:"adva.#{type}.links.#{action}", :default => :"adva.resources.links.#{action}")
-      text = t(text) if text.is_a?(Symbol)
-      text
-    end
-
-    def resource_delete_options(type, options)
-      type = type.to_s.gsub('/', '_').pluralize
-      message = options.delete(:confirm)
-      message ||= t(:"adva.#{type}.confirm_delete", :default => :"adva.resources.confirm_delete")
-      message = t(message) if message.is_a?(Symbol)
-      { :data => { :confirm => message }, :method => :delete }
-    end
+  def resource_delete_options(type, options)
+    type = type.to_s.gsub('/', '_').pluralize
+    message = options.delete(:confirm)
+    message ||= t(:"adva.#{type}.confirm_delete", :default => :"adva.resources.confirm_delete")
+    message = t(message) if message.is_a?(Symbol)
+    { :data => { :confirm => message }, :method => :delete }
+  end
 end
