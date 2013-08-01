7690728d522b546ccbb752c78850fe3a18aebd78
Micah Geisel <micah@botandrose.com>
2013-07-31 17:11:37 -0700

    require comment author name and email again.

--- adva_comments/app/models/comment.rb
+++ adva_comments/app/models/comment.rb
@@ -41,7 +41,7 @@ class Comment < ActiveRecord::Base
 
   composed_of :author, :class_name => "User", :mapping => [ %w(author_name name), %w(author_email email) ]
 
-  validates_presence_of :body, :commentable
+  validates_presence_of :body, :commentable, :author_name, :author_email
 
   before_validation :set_owners
   before_create :authorize_commenting
