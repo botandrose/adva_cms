2d297d358113bd1b6843dcdbf7a8073f8acbf5ee
Micah Geisel <micah@botandrose.com>
2013-08-19 10:54:24 -0700

    remove user registration.

--- adva_user/config/routes.rb
+++ adva_user/config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   get "login" => "session#new"
   delete "logout" => "session#destroy"
 
-  get "signup" => "user#new"
-
   resource :session,     :controller => "session"
   resource :password,    :controller => "password"
-  resource :user,        :controller => "user" do
-    get "verify", :on => :member
-    get "verification_sent", :on => :collection
-  end
 
   namespace :admin do
     resources :users
