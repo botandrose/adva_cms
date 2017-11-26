a62daa42baf02b74cd6c6ee2686b2cee3f91c0ac
Micah Geisel <micah@botandrose.com>
2017-11-26 10:16:47 -0800

    clean up youtube plugin formatting.

--- adva_fckeditor/app/assets/javascripts/adva_fckeditor/fckeditor_plugins/youtube/youtube.js
+++ adva_fckeditor/app/assets/javascripts/adva_fckeditor/fckeditor_plugins/youtube/youtube.js
@@ -1,8 +1,8 @@
-var dialog		= window.parent ;
-var oEditor		= dialog.InnerDialogLoaded() ;
-var FCK			= oEditor.FCK ;
-var FCKLang		= oEditor.FCKLang ;
-var FCKConfig	= oEditor.FCKConfig ;
+var dialog     = window.parent;
+var oEditor    = dialog.InnerDialogLoaded();
+var FCK        = oEditor.FCK;
+var FCKLang    = oEditor.FCKLang;
+var FCKConfig  = oEditor.FCKConfig;
 
 //security RegExp
 var REG_SCRIPT = new RegExp("< *script.*>|< *style.*>|< *link.*>|< *body .*>", "i");
@@ -17,105 +17,98 @@ var REG_NEWLINE = new RegExp("\x0d|\x0a", "i");
 //#### Dialog Tabs
 
 // Set the dialog tabs.
-dialog.AddTab( 'Info', oEditor.FCKLang.DlgInfoTab ) ;
+dialog.AddTab('Info', oEditor.FCKLang.DlgInfoTab);
 
 // Get the selected flash embed (if available).
-var oFakeImage = FCK.Selection.GetSelectedElement() ;
-var oEmbed ;
+var oFakeImage = FCK.Selection.GetSelectedElement();
+var oEmbed;
 
-window.onload = function()
-{
-	// Translate the dialog box texts.
-	oEditor.FCKLanguageManager.TranslatePage(document) ;
+window.onload = function() {
+  // Translate the dialog box texts.
+  oEditor.FCKLanguageManager.TranslatePage(document);
 
-	dialog.SetAutoSize( true ) ;
+  dialog.SetAutoSize(true);
 
-	// Activate the "OK" button.
-	dialog.SetOkButton( true ) ;
+  // Activate the "OK" button.
+  dialog.SetOkButton(true);
 
-	SelectField( 'txtUrl' ) ;
+  SelectField('txtUrl');
 }
 
 //#### The OK button was hit.
-function Ok()
-{
-	if ( GetE('txtUrl').value.length == 0 )
-	{
-		dialog.SetSelectedTab( 'Info' ) ;
-		GetE('txtUrl').focus() ;
+function Ok() {
+  if (GetE('txtUrl').value.length == 0) {
+    dialog.SetSelectedTab('Info');
+    GetE('txtUrl').focus();
 
-		alert( oEditor.FCKLang.DlgYouTubeCode ) ;
+    alert(oEditor.FCKLang.DlgYouTubeCode);
 
-		return false ;
-	}
-	
-	// check security
-	if (checkCode(GetE('txtUrl').value) == false) {
-		alert( oEditor.FCKLang.DlgYouTubeSecurity ) ;
-		return false;
-	}
-	
-    oEditor.FCKUndo.SaveUndoStep() ;
-    if ( !oEmbed )
-	{
-		oEmbed		= FCK.EditorDocument.createElement( 'EMBED' ) ;
-		oFakeImage  = null ;
-	}
-	UpdateEmbed( oEmbed ) ;
+    return false;
+  }
 
-	if ( !oFakeImage )
-	{
-		oFakeImage	= oEditor.FCKDocumentProcessor_CreateFakeImage( 'FCK__Flash', oEmbed ) ;
-		oFakeImage.setAttribute( '_fckflash', 'true', 0 ) ;
-		oFakeImage	= FCK.InsertElement( oFakeImage ) ;
-	}
+  // check security
+  if (checkCode(GetE('txtUrl').value) == false) {
+    alert(oEditor.FCKLang.DlgYouTubeSecurity);
+    return false;
+  }
 
-    oEditor.FCKEmbedAndObjectProcessor.RefreshView( oFakeImage, oEmbed ) ;
+  oEditor.FCKUndo.SaveUndoStep();
+  if (!oEmbed) {
+    oEmbed    = FCK.EditorDocument.createElement('EMBED');
+    oFakeImage  = null;
+  }
+  UpdateEmbed(oEmbed);
 
-	return true ;
+  if (!oFakeImage) {
+    oFakeImage = oEditor.FCKDocumentProcessor_CreateFakeImage('FCK__Flash', oEmbed);
+    oFakeImage.setAttribute('_fckflash', 'true', 0);
+    oFakeImage = FCK.InsertElement(oFakeImage);
+  }
+
+  oEditor.FCKEmbedAndObjectProcessor.RefreshView(oFakeImage, oEmbed);
+
+  return true;
 }
 
-function UpdateEmbed( e )
-{
-	var youtubeUrl 	= GetE('txtUrl').value;
+function UpdateEmbed(e) {
+  var youtubeUrl = GetE('txtUrl').value;
   var youtubeId = '';
   if(result = ('"' + youtubeUrl + '"').match(/\/([^/"]+)"/)) {
     youtubeId = result[1];
   }
-	
-	SetAttribute( e, 'type'			, 'application/x-shockwave-flash' ) ;
-	SetAttribute( e, 'pluginspage'	, 'http://www.macromedia.com/go/getflashplayer' ) ;
-	SetAttribute( e, 'allowscriptaccess'	, 'always' ) ;
-	
-  SetAttribute( e, 'src'		, 'http://www.youtube.com/v/'+youtubeId+'?version=3&autoplay=1') ;
-	
-	SetAttribute( e, "width" 		, GetE('txtWidth').value == '' ? 640 : GetE('txtWidth').value ) ;
-	SetAttribute( e, "height"		, GetE('txtHeight').value == '' ? 390 : GetE('txtHeight').value ) ;
+
+  SetAttribute(e, 'type', 'application/x-shockwave-flash');
+  SetAttribute(e, 'pluginspage', 'http://www.macromedia.com/go/getflashplayer');
+  SetAttribute(e, 'allowscriptaccess', 'always');
+
+  SetAttribute(e, 'src', 'http://www.youtube.com/v/'+youtubeId+'?version=3&autoplay=1');
+
+  SetAttribute(e, "width", GetE('txtWidth').value == '' ? 640 : GetE('txtWidth').value);
+  SetAttribute(e, "height", GetE('txtHeight').value == '' ? 390 : GetE('txtHeight').value);
 }
 
-function checkCode(code)
-{
-	if (code.search(REG_SCRIPT) != -1) {
-		return false;
-	}
-	
-	if (code.search(REG_PROTOCOL) != -1) {
-		return false;
-	}
+function checkCode(code) {
+  if (code.search(REG_SCRIPT) != -1) {
+    return false;
+  }
 
-	if (code.search(REG_CALL_SCRIPT) != -1) {
-		return false;
-	}
+  if (code.search(REG_PROTOCOL) != -1) {
+    return false;
+  }
 
-	if (code.search(REG_EVENT) != -1) {
-		return false;
-	}
+  if (code.search(REG_CALL_SCRIPT) != -1) {
+    return false;
+  }
 
-	if (code.search(REG_AUTH) != -1) {
-		return false;
-	}
+  if (code.search(REG_EVENT) != -1) {
+    return false;
+  }
 
-	if (code.search(REG_NEWLINE) != -1) {
-		return false;
-	}
+  if (code.search(REG_AUTH) != -1) {
+    return false;
+  }
+
+  if (code.search(REG_NEWLINE) != -1) {
+    return false;
+  }
 }
