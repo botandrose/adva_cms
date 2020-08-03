bc0e9241a4e45da78bae9db1774d34a9baf405f5
Micah Geisel <micah@botandrose.com>
2020-08-03 13:30:18 -0700

    need this, too.

--- adva_comments/config/initializers/controllers.rb
+++ adva_comments/config/initializers/controllers.rb
@@ -8,24 +8,6 @@ ActiveSupport::Reloader.to_prepare do
         format.atom { render :template => 'comments/comments', :layout => false }
       end
     end
-
-    private
-
-    # goofy gauranteed-to-be-unique spam filter
-    # test for variable sent along with the form. this variable is populated in the form via javascript after one second.
-    def are_you_a_human_or_not
-      if honeypot_filled? or not_a_real_browser?
-        head :not_found and return false if Rails.env.production?
-      end
-    end
-
-    def honeypot_filled?
-      (params[:comment] || {}).delete(:url).present?
-    end
-
-    def not_a_real_browser?
-      params[:are_you_a_human_or_not] != "if you prick me, do i not bleed?"
-    end
   end
   
   Admin::BaseController.helper :comments, :'admin/comments'
