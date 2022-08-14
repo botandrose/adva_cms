96ebd15cffd252cd2fefaa1c4c7196f27c606376
Micah Geisel <micah@botandrose.com>
2022-08-14 12:53:07 +0200

    404 on bad commentable_type and refactor.

--- adva_comments/app/controllers/comments_controller.rb
+++ adva_comments/app/controllers/comments_controller.rb
@@ -19,7 +19,6 @@ class CommentsController < BaseController
   before_action :set_section
   before_action :set_comment, :only => [:show, :update, :destroy]
   before_action :set_commentable, :only => [:show, :preview, :create]
-  before_action :set_comment_params, :only => [:preview, :create]
 
   before_action :are_you_a_human_or_not, :only => :create
 
@@ -29,78 +28,84 @@ class CommentsController < BaseController
   end
 
   def preview
-    @comment = @commentable.comments.build(params[:comment])
+    @comment = @commentable.comments.build(comment_params)
     @comment.send(:process_filters)
     render :layout => false
   end
 
   def create
-    @comment = @commentable.comments.build(params[:comment])
+    @comment = @commentable.comments.build(comment_params)
     if @comment.save
       trigger_events @comment
       CommentMailer.comment_notification(@comment).deliver_later
       if current_user.anonymous?
-        flash[:notice] = "Your comment is being reviewed, and will be posted shortly. Thank you for commenting!"
+        flash.notice = "Your comment is being reviewed, and will be posted shortly. Thank you for commenting!"
       else
         @comment.update_column :approved, true
-        flash[:notice] = "You're an admin, so your comment is being posted immediately! Refresh the page to see it."
+        flash.notice = "You're an admin, so your comment is being posted immediately! Refresh the page to see it."
       end
       respond_to do |format|
         format.html { redirect_to "#{request.env['HTTP_REFERER']}#comments" }
-        format.js { render :json => true }
+        format.js { render json: true }
       end
     else
       flash[:error] = @comment.errors.full_messages.to_sentence # TODO hu.
       respond_to do |format|
         format.html { redirect_to "#{request.env['HTTP_REFERER']}#comments" }
-        format.js { render :json => false }
+        format.js { render json: false }
       end
     end
   end
 
   def update
-    if @comment.update(params[:comment])
+    if @comment.update(comment_params)
       trigger_events(@comment)
-      flash[:notice] = t(:'adva.comments.flash.update.success')
-      render :json => true
+      flash.notice = t(:'adva.comments.flash.update.success')
+      render json: true
     else
       set_commentable
       flash[:error] = @comment.errors.full_messages.to_sentence
-      render :json => false
+      render json: false
     end
   end
 
   def destroy
     @comment.destroy
     trigger_events @comment
-    flash[:notice] = t(:'adva.comments.flash.destroy.success')
-    redirect_to "/"
+    redirect_to "/", notice: t(:'adva.comments.flash.destroy.success')
   end
 
   protected
 
-    def set_comment
-      @comment = Comment.find params[:id]
-    end
+  def comment_params
+    params.require(:comment).permit(
+      :author_email,
+      :author_name,
+      :body,
+      :commentable_id,
+      :commentable_type,
+    ).merge(site_id: @commentable.site_id, section_id: @commentable.section_id)
+  end
 
-    def set_commentable
-      @commentable = if @comment
-        @comment.commentable
-      else
-        params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_commentable
+    @commentable = if @comment
+      @comment.commentable or raise ActiveRecord::RecordNotFound
+    else
+      begin
+        klass = params.dig(:comment, :commentable_type).constantize
+        raise NameError unless klass.has_many_comments?
+      rescue NameError
+        raise ActiveRecord::RecordNotFound
       end
-      raise ActiveRecord::RecordNotFound unless @commentable
+      klass.find(params.dig(:comment, :commentable_id))
     end
+  end
 
-    def set_comment_params
-      return unless params[:comment]
-
-      params[:comment].delete(:approved)  # TODO use attr_protected api?
-      params[:comment].merge!(:site_id => @commentable.site_id,
-                              :section_id => @commentable.section_id)
-    end
-
-    def current_resource
-      @comment || @commentable
-    end
+  def current_resource
+    @comment || @commentable
+  end
 end
