3e75a41766c8eaa35b9e5186cb29cf31033d2c3e
Micah Geisel <micah@botandrose.com>
2023-05-07 15:20:42 +0200

    reimplement tooltips with title attribute.

--- app/assets/javascripts/adva_cms/application.js
+++ app/assets/javascripts/adva_cms/application.js
@@ -1,9 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require adva_cms/base
-//= require adva_cms/jquery/jquery.qtip
 //= require adva_cms/jquery/jquery-lowpro
-//= require adva_cms/jquery.common
 //= require adva_cms/admin/jquery.admin
 //= require adva_cms/jquery/jquery.tablednd_0_5
 //= require adva_cms/jquery/jquery.table_tree
