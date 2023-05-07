24279058da205ef1699dbf51d60bec99786c4f5c
Micah Geisel <micah@botandrose.com>
2023-05-07 16:49:18 +0200

    remove jquery from cookie flash.

--- app/assets/javascripts/adva_cms/application.js
+++ app/assets/javascripts/adva_cms/application.js
@@ -1,8 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 
-//= require adva_cms/cookie
-//= require adva_cms/jquery.flash
+//= require adva_cms/cookie.flash
 
 //= require adva_cms/admin/jquery.admin
 //= require adva_cms/admin/jquery.table_tree
