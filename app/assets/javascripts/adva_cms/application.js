9b6cd9b75735ddc94dbf80482ee7e4c7273b20c4
Micah Geisel <micah@botandrose.com>
2023-05-07 15:50:12 +0200

    reorg.

--- app/assets/javascripts/adva_cms/application.js
+++ app/assets/javascripts/adva_cms/application.js
@@ -1,11 +1,20 @@
 //= require jquery
 //= require jquery_ujs
-//= require adva_cms/base
-//= require adva_cms/jquery/jquery-lowpro
+
+//= require adva_cms/cookie
+//= require adva_cms/jquery.flash
+//= require adva_cms/jquery.roles
+
 //= require adva_cms/admin/jquery.admin
-//= require adva_cms/jquery/jquery.tablednd_0_5
-//= require adva_cms/jquery/jquery.table_tree
 //= require adva_cms/admin/jquery.table_tree
 //= require adva_cms/admin/toggle_excerpt
 //= require adva_cms/admin/ckeditor
 //= require has_filter/jquery.filter
+
+(function($) {
+  $.ajaxSetup({ 
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
+    } 
+  });
+})(jQuery);
