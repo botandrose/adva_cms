a1cf6d0b3877b4eb5faab6e38803aca9a0e7d3ae
Micah Geisel <micah@botandrose.com>
2023-05-07 15:48:13 +0200

    rewrite toggle excerpt without jquery.

--- app/assets/javascripts/adva_cms/application.js
+++ app/assets/javascripts/adva_cms/application.js
@@ -6,6 +6,6 @@
 //= require adva_cms/jquery/jquery.tablednd_0_5
 //= require adva_cms/jquery/jquery.table_tree
 //= require adva_cms/admin/jquery.table_tree
-//= require adva_cms/admin/jquery.article
+//= require adva_cms/admin/toggle_excerpt
 //= require adva_cms/admin/ckeditor
 //= require has_filter/jquery.filter
