0dbc91f0316772e402c8b5dd2555b7636cf0cf4a
Micah Geisel <micah@botandrose.com>
2023-05-07 15:06:30 +0200

    refactor toggle visibility from jQuery to pure CSS.

--- app/assets/javascripts/adva_cms/jquery.common.js
+++ app/assets/javascripts/adva_cms/jquery.common.js
@@ -1,17 +1,5 @@
 (function($) {
-  window.Form = {
-    toggleDraft: function() {
-      if($(this).attr('checked')) {
-        $('#publish_date_wrapper').hide();
-      } else {
-        $('#publish_date_wrapper').show();
-      }
-    }
-  }
-
   $(document).ready(function() {
-    $('#toggle_draft').click(Form.toggleDraft);
-
     if($(".hint").size() > 0) {
       $(".hint").each(function() {
         if(!$(this).hasClass('text_only')) {
