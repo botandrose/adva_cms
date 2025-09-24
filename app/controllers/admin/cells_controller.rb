8c75b69c63138546bbc158608ce86839be82d317
Micah Geisel <micah@botandrose.com>
2025-09-24 14:53:44 -0400

    rm /admin/cells because this is provided by the nacelle gem now.

--- app/controllers/admin/cells_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::CellsController < Admin::BaseController
-  def index
-    @cells = []
-    render plain: ""
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,6 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :cells, :only => :index
 
     resources :users
   end
