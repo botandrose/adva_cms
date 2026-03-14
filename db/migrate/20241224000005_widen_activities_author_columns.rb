7e84ebff7d0e12c7535c9c53e0a90937e34991c1
Micah Geisel <micah@botandrose.com>
2026-03-14 15:30:25 -0600

    widen activities author_name and author_email columns to 128 chars

---
+++ db/migrate/20241224000005_widen_activities_author_columns.rb
@@ -0,0 +1,6 @@
+class WidenActivitiesAuthorColumns < ActiveRecord::Migration[7.0]
+  def change
+    change_column :activities, :author_name, :string, limit: 128
+    change_column :activities, :author_email, :string, limit: 128
+  end
+end
